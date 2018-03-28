09741735c84a216ea0c5e780426745f1eca4abb3
Kyle Horsch <krhorsch@yahoo.com>
2018-03-28 19:35:55 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,7 +9,19 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-
+    @figure = Figure.create(name: params["figure"]["name"])
+    @figure.title_ids = params["figure"]["title_ids"]
+    @figure.landmark_ids = params["figure"]["landmark_ids"]
+    if params["title"]["name"]
+      @title = Title.create(name: params["title"]["name"])
+      @figure.titles << @title
+    end
+    if params["landmark"]["name"]
+      @landmark = Landmark.create(name: params["landmark"]["name"])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 
   get '/figures/:id/edit' do
@@ -17,8 +29,19 @@ class FiguresController < ApplicationController
     erb :"/figures/edit"
   end
 
-  patch '/figures' do
-
+  patch '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    @figure.update(name: params["figure"]["name"], title_ids: params["figure"]["title_ids"], landmark_ids: params["figure"]["landmark_ids"])
+    if params["title"]["name"]
+      @title = Title.create(name: params["title"]["name"])
+      @figure.titles << @title
+    end
+    if params["landmark"]["name"]
+      @landmark = Landmark.create(name: params["landmark"]["name"])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 
   get '/figures/:id' do
